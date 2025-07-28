49dc50cfbd1ec4e51d68bd8c7634af601ef43683
Felipe Santana da Silva <felipe.ssilva@valid.com>
2025-07-28 20:38:23 -0300

    Criando componentes

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Text from "./components/text"
+
+function App() {
+
+  return (
+    <>
+      <Text variant="body-md-bold" className="text-pink-base">Todo List</Text>
+    </>
+  )
+}
+
+export default App
